74ce1f379ca6feb667b060c044e26807c86fa2f7
alexisandries <131961393+alexisandries@users.noreply.github.com>
2024-04-04 00:43:10 +0200

    Update source_code.py

--- source_code.py
+++ source_code.py
@@ -162,7 +162,7 @@ def main():
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
         
-        if prompt := st.chat_input("Yo bro"):
+        if prompt := st.chat_input():
             st.session_state.messages.append({"role": "user", "content": prompt})
             with st.chat_message("user"):
                 st.markdown(prompt)
